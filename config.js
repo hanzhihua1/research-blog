f85d3f936dde304d209ce3ede304f7a78097bc26
Zhi Han <zhihuahan72@gmail.com>
2020-02-02 23:26:53 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://hanzhihua1.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'Notes by Zhi Han',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
+  useKatex: true,
   menu: [
     {
       label: 'Articles',
@@ -25,15 +25,15 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Zhi Han',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Notes for research',
     contacts: {
-      email: '',
+      email: 'zhihuahan72@gmail.com',
       facebook: '#',
       telegram: '#',
       twitter: '#',
-      github: '#',
+      github: 'hanzhihua1',
       rss: '',
       vkontakte: '',
       linkedin: '#',
